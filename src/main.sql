ed2911e5461c58ffa41e8577c631196d5e750a65
IamThiago-IT <thiagodossantos315@gmail.com>
2023-11-02 18:25:22 -0300

    create table client

--- src/main.sql
+++ src/main.sql
@@ -1,2 +1,16 @@
 -- criação do banco de dados
 create database lol;
+
+-- Criação da tabela Cliente
+
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    endereco TEXT,
+    telefone VARCHAR(20),
+    senha VARCHAR(64) NOT NULL,
+    sal VARCHAR(16) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT NOW()
+);
