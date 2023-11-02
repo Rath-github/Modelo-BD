a326b8da61ae662ddbc9532a5eb6814aefea379a
IamThiago-IT <thiagodossantos315@gmail.com>
2023-11-02 18:29:15 -0300

    create table employees

--- src/main.sql
+++ src/main.sql
@@ -14,3 +14,16 @@ CREATE TABLE clientes (
     sal VARCHAR(16) NOT NULL,
     data_cadastro TIMESTAMP DEFAULT NOW()
 );
+
+-- Criação da tabela Funcionarios
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    endereco TEXT,
+    telefone VARCHAR(20),
+    senha VARCHAR(64) NOT NULL,
+    sal VARCHAR(16) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT NOW()
+);
